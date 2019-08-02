91f1528df16de27802440bfe9b1d2c6e22a31d28
RexLeong <rex12425@gmail.com>
2019-08-03 00:25:51 +0800

    3 New and 2 Defined:  - New: function `close` to inform PostManager to close the MySQL Server Connection  - imported `os.path` for path modfication  - imported `exit` and `exc_info` from module `sys`  - Defined: Method `GetPostInfoById` and method `GetPostById`

--- pyposts.py
+++ pyposts.py
@@ -1,9 +1,14 @@
+# pyramid web framework
 from pyramid.config import Configurator
 from wsgiref.simple_server import make_server
 # mysql connector to interact with MySQL Server
 import mysql.connector as sql
 # required JSON to return post content
 from json import JSONEncoder
+# for path modification
+from os.path import dirname,abspath
+# for error output
+from sys import exc_info,exit
 
 # object aren't necessary right now, commented
 """
@@ -49,7 +54,7 @@ class PostManager:
 
     Find post content according to the type and ID provided
     id = The Post ID
-    type = The requested content type, there are four types:
+    Type = The requested content type, there are four types:
         1. title
         2. author (The ID of the post author)
         3. date (Date of the post posted, see below for more info)
@@ -57,7 +62,21 @@ class PostManager:
 
         for date, only digit and underscore is accepted, example: 09_06_2019 if the date is 9 June 2019
     """
-    def GetPostInfoById(self, id, type)
+    def GetPostInfoById(self, id, Type):
+        # check if MySQL server connection is still established
+        if self.mysql_conn.is_connected:
+            cursor = self.mysql_conn.cursor()
+            cursor.execute("SELECT {} FROM Posts WHERE id={}".format(Type, str(id)))
+            content = cursor.fetchone()
+            # when bool(content) == True, means value is found
+            if bool(content):
+                return content[0]
+            # otherwise, None will be returned because result wasn't found
+            else:
+                return None
+        else:
+            # raise error if MySQL Server has disconnected
+            raise Exception("MySQL Server has been disconnected.")
 
 
     """
@@ -71,13 +90,67 @@ class PostManager:
             2. Default is False
     
     Value will be returned:
-    1. post_title (The post's title)
+    1. title (The post's title)
+    2. content (The post's content)
     2. posted_date (The posted date)
     3. last_modified (The last day of the post modified)
-    4. post_author (The ID of the post's author)
+    4. author (The ID of the post's author)
     5. modified (does the post was modified? true or false only)
     """
-    def GetPostById(self, id, json=False)
+    def GetPostById(self, id, json=False):
+        if self.mysql_conn.is_connected():
+            cursor = self.mysql_conn.cursor()
+            cursor.execute("SELECT title,posted_date,last_modifed,author,modified FROM Posts WHERE id={}".format(str(id)))
+            result = cursor.fetchone()
+            if bool(result):
+                try:
+                    # this post's content storage is TEMPORARY only, planned to archive whole post content and infos
+                    # post content's file is saved with id as the name of file with extension '.pypt'
+                    #
+                    # then all post files is stored in folder 'posts' from parent folder 'pyposts' which is under the same directory
+                    post_content = open(dirname(abspath(__file__))+'/pyposts/posts/{}.pypt'.format(str(id)),'r').read()
+                
+                # if current process's user can't access to file, return None and output Error MSG
+                except FileNotFoundError:
+                    print(exc_info())
+                    return None
+                
+                # if current process's user can't access to file, return None and output Error MSG
+                except PermissionError:
+                    print(exc_info())
+                    return None
+ 
+                # create a dictionary for post infos
+                post = dict()
+                # assign values to dictionary via assignments
+                # post id
+                post['id'] = id
+                # post title
+                post['title'] = result[0]
+                # post content
+                post['content'] = post_content
+                # posted date
+                post['posted_date'] = result[1]
+                # last modified date
+                post['last_modified'] = result[2]
+                # post author
+                post['author'] = result[3]
+                # does post modified?
+                post['modified'] = result[4]
+
+                # if json=True, encode to json string
+                if json:
+                    post_json = JSONEncoder(indent=4).encode(post)
+                    return post_json
+
+                else:
+                    # otherwise just return the python dictionary
+                    return post
+            else:
+                # same as method above, None will be returned if result isn't found
+                return None
+        else:
+            raise Exception("MySQL Server has been disconnected.")
 
 
     """
@@ -108,3 +181,13 @@ class PostManager:
                                  user=self.mysql_user,
                                  passwd=self.mysql_passwd,
                                  database=self.mysql_db)
+    
+    """
+    method close, close the PostManager
+
+    Actually this is just a method to tell that PostManager can be stopped
+    what it does was just close the MySQL Server connection object
+    This must be called before closing your program
+    """
+    def close(self):
+        self.mysql_conn.close()
